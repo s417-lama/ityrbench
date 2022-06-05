a1cdbf416d9d995586ee667781396fe10b16d443
Shumpei Shiina <shiina@eidos.i.u-tokyo.ac.jp>
2022-06-06 02:57:01 +0900

    improve work-first-based fence elimination

--- ityr.hpp
+++ ityr.hpp
@@ -2,51 +2,105 @@
 #include "pcas/pcas.hpp"
 
 template <typename P>
-class iro;
+class ityr_if;
+
+template <typename P>
+class iro_if;
 
 template <typename P, size_t MaxTasks, bool SpawnLastTask>
 class ito_group {
-  using iro = iro<P>;
+  using iro = iro_if<P>;
 
   madm::uth::thread<void> tasks_[MaxTasks];
+  bool synched_[MaxTasks];
+  uint64_t initial_rank;
   size_t n_ = 0;
 
 public:
-  ito_group() {}
+  ito_group() { initial_rank = ityr_if<P>::rank(); }
 
   template <typename F, typename... Args>
   void run(F f, Args... args) {
     assert(n_ < MaxTasks);
     if (SpawnLastTask || n_ < MaxTasks - 1) {
-      iro::release();
-      auto p_th = &tasks_[n_++];
-      new (p_th) madm::uth::thread<void>{[=]() {
-        if (!P::work_first) {
+      auto p_th = &tasks_[n_];
+      if (!P::work_first) {
+
+        iro::release();
+        new (p_th) madm::uth::thread<void>{[=] {
+          iro::acquire();
+          f(args...);
+          iro::release();
+        }};
+        iro::acquire();
+
+      } else {
+
+        iro::release();
+        new (p_th) madm::uth::thread<void>{};
+        synched_[n_] = p_th->spawn_aux([=]() {
+          if (!P::work_first) {
+            iro::acquire();
+          }
+          f(args...);
+        }, std::make_tuple(), [=] (bool parent_popped) {
+          // on-die callback
+          if (!parent_popped) {
+            iro::release();
+          }
+        });
+        if (!synched_[n_]) {
           iro::acquire();
         }
-        f(args...);
-        iro::release();
-      }};
-      if (!P::work_first || !p_th->synched()) {
-        iro::acquire();
+
       }
+      n_++;
     } else {
       f(args...);
     }
   }
 
   void wait() {
-    iro::release();
-    for (size_t i = 0; i < n_; i++) {
-      tasks_[i].join();
+    if (!P::work_first) {
+
+      iro::release();
+      for (size_t i = 0; i < n_; i++) {
+        tasks_[i].join();
+      }
+      iro::acquire();
+
+    } else {
+
+      bool blocked = false;
+      for (size_t i = 0; i < n_; i++) {
+        tasks_[i].join_aux(0, [&blocked] {
+          // on-block callback
+          if (!blocked) {
+            iro::release();
+            blocked = true;
+          }
+        });
+      }
+      bool all_synched = true;
+      for (size_t i = 0; i < n_; i++) {
+        if (!synched_[i]) {
+          all_synched = false;
+          break;
+        }
+      }
+      if (initial_rank != ityr_if<P>::rank() ||
+          (!all_synched || blocked)) {
+        // FIXME: (all_synched && blocked) is true only for root tasks
+        iro::acquire();
+      }
+
     }
-    iro::acquire();
     n_ = 0;
   }
 };
 
 template <typename P>
-class iro {
+class iro_if {
   static pcas::pcas& pc(size_t cache_size = 0) {
     static pcas::pcas g_pc(cache_size);
     return g_pc;
@@ -91,11 +145,12 @@ public:
 };
 
 template <typename P>
-struct ityr_if {
+class ityr_if {
+public:
   template <size_t MaxTasks, bool SpawnLastTask = false>
   using ito_group = ito_group<P, MaxTasks, SpawnLastTask>;
 
-  using iro = iro<P>;
+  using iro = iro_if<P>;
 
   static uint64_t rank() {
     return madm::uth::get_pid();
