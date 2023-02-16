54a970a05ce29381dd99ace84617e46166b6af3f
Shumpei Shiina <shiina@eidos.ic.i.u-tokyo.ac.jp>
2023-02-16 18:09:38 +0900

    update

--- run_common.bash
+++ run_common.bash
@@ -95,7 +95,7 @@ case $KOCHI_MACHINE in
     ;;
   squid-c)
     export UCX_TLS=rc_x,self,sm
-    export OMPI_MCA_mca_base_env_list="LD_PRELOAD;UCX_NET_DEVICES;UCX_TLS;"
+    export OMPI_MCA_mca_base_env_list="TERM;LD_PRELOAD;UCX_NET_DEVICES;UCX_TLS;"
     ityr_mpirun() {
       local n_processes=$1
       local n_processes_per_node=$2
@@ -103,14 +103,13 @@ case $KOCHI_MACHINE in
       trap "compgen -G ${STDOUT_FILE}.* && tail -n +1 \$(ls ${STDOUT_FILE}.* -v) > $STDOUT_FILE && rm ${STDOUT_FILE}.*" EXIT
 
       $MPIEXEC -n $n_processes -N $n_processes_per_node \
-        --bind-to core \
         --output file=$STDOUT_FILE \
         --prtemca ras simulator \
         --prtemca plm_ssh_agent ssh \
         --prtemca plm_ssh_args " -i /sqfs/home/v60680/sshd/ssh_client_rsa_key -o StrictHostKeyChecking=no -p 50000 -q" \
         --hostfile $NQSII_MPINODES \
         --mca osc_ucx_acc_single_intrinsic true \
-        -- setarch $(uname -m) --addr-no-randomize "${@:3}"
+        setarch $(uname -m) --addr-no-randomize "${@:3}"
     }
     ;;
   *)
